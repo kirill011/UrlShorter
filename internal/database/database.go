12c89bcf3482e96089cbd4d7c129b4ecbd326366
kirill <kirill.petrov.2001@list.ru>
2024-03-06 10:09:06 +0300

    remove !

--- internal/database/database.go
+++ internal/database/database.go
@@ -18,8 +18,7 @@ func NewDatabase(cfg *config.Config) database {
 	dFlag := *flag.Bool("d", false, "В зависимости от флага -d данные будут храниться либо в памяти(если нет флага -d), либо в PostgreSQl(если флаг присутствует)")
 	flag.Parse()
 
-	//Убрать !
-	if !dFlag {
+	if dFlag {
 		return postgres.NewPostgres(cfg.Host, cfg.User, cfg.Password, cfg.Port, cfg.SslMode, cfg.TimeZone, cfg.DbName)
 	}
 
